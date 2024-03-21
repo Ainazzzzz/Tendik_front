5cdd56e3e6af35aed01226b176b32060232a7df5
Ainazik <ainazik_toktomamatova@epam.com>
2024-03-22 01:52:00 +0600

    random

--- src/i18n.js
+++ src/i18n.js
@@ -3,17 +3,39 @@ import { initReactI18next } from 'react-i18next'
 import LanguageDetector from 'i18next-browser-languagedetector'
 import Backend from 'i18next-http-backend'
 
+const Languages = ['ru', 'ky']
+
 i18n
    .use(Backend)
    .use(LanguageDetector)
    .use(initReactI18next)
    .init({
-      debug: true,
+      lng: 'en',
+      react: {
+         useSuspense: false,
+         wait: true,
+      },
+      fallbackLng: 'en',
       supportedLngs: ['ru', 'ky'],
-      fallbackLng: 'ru',
+
+      debug: false,
+      whitelist: Languages,
       interpolation: {
          escapeValue: false,
       },
+      nsSeperator: false,
+      keySeperator: false,
+      backend: {
+         loadPath: '/locales/{{lng}}/{{ns}}.json',
+      },
    })
+// .init({
+//    debug: true,
+//    supportedLngs: ['ru', 'ky'],
+//    fallbackLng: 'ru',
+//    interpolation: {
+//       escapeValue: false,
+//    },
+// })
 
 export default i18n
